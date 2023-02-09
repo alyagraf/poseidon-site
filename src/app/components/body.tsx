6102024025746297a955db64e6c9bc7068453188
wmoody6293 <wmoody6293@gmail.com>
2023-02-09 14:34:49 -0500

    update all creator images to also be links to our linkedin pages

--- src/app/components/body.tsx
+++ src/app/components/body.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import github from "../../../public/github.png";
-import hero from "../../../public/hero.gif";
-import Link from "next/link";
+import Image from 'next/image';
+import github from '../../../public/github.png';
+import hero from '../../../public/hero.gif';
+import Link from 'next/link';
 
 export default function Body() {
   return (
@@ -9,11 +9,11 @@ export default function Body() {
       <section className="px-4 pt-40 pb-20 mx-auto max-w-7xl">
         <div className="w-full mx-auto text-left md:w-10/12 xl:w-10/12 md:text-start">
           <h1 className="mb-6 text-4xl font-extrabold leading-none tracking-normal text-slate-100 md:text-6xl md:tracking-tight">
-            Empower your Kubernetes with{" "}
+            Empower your Kubernetes with{' '}
             <span className="block w-full text-transparent bg-clip-text bg-gradient-to-r from-sky-500 to-green-400 lg:inline">
-              {" "}
-              Cluster Visualizer & Cost Analysis{" "}
-            </span>{" "}
+              {' '}
+              Cluster Visualizer & Cost Analysis{' '}
+            </span>{' '}
             in one single place.
           </h1>
           <p className="px-0 mb-6 text-lg text-gray-600 md:text-xl lg:px-30">
@@ -29,7 +29,9 @@ export default function Body() {
               <a
                 aria-label="github"
                 href="https://github.com/oslabs-beta/poseidon"
-                className="p-4 border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-full duration-300 hover:border-sky-500 hover:shadow-lg hover:shadow-cyan-600/20 dark:hover:border-cyan-300/30">
+                target="_blank"
+                className="p-4 border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-full duration-300 hover:border-sky-500 hover:shadow-lg hover:shadow-cyan-600/20 dark:hover:border-cyan-300/30"
+              >
                 <div className="flex justify-center space-x-4">
                   <Image
                     className="invert"
@@ -46,7 +48,9 @@ export default function Body() {
               <a
                 aria-label="video tutorial"
                 href="https://youtu.be/hqeGqh0ksPg"
-                className="p-4 border border-gray-200 dark:bg-gray-800  dark:border-gray-700 rounded-full duration-300 hover:border-green-400 hover:shadow-lg hover:shadow-lime-600/20 dark:hover:border-green-300/30">
+                target="_blank"
+                className="p-4 border border-gray-200 dark:bg-gray-800  dark:border-gray-700 rounded-full duration-300 hover:border-green-400 hover:shadow-lg hover:shadow-lime-600/20 dark:hover:border-green-300/30"
+              >
                 <div className="flex justify-center space-x-4">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -54,7 +58,8 @@ export default function Body() {
                     viewBox="0 0 24 24"
                     strokeWidth="1.5"
                     stroke="currentColor"
-                    className="w-6 h-6 ">
+                    className="w-6 h-6 "
+                  >
                     <path
                       strokeLinecap="round"
                       d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
--- src/app/components/creatorsCompo.tsx
+++ src/app/components/creatorsCompo.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
-import alina from "../../../public/alina.jpg";
-import will from "../../../public/will.jpg";
-import ember from "../../../public/ember.jpg";
-import josh from "../../../public/josh.jpg";
+import Image from 'next/image';
+import alina from '../../../public/alina.jpg';
+import will from '../../../public/will.jpg';
+import ember from '../../../public/ember.jpg';
+import josh from '../../../public/josh.jpg';
+import Link from 'next/link';
 
 export default function Creators() {
   return (
@@ -18,101 +19,114 @@ export default function Creators() {
               optimize their Kubernetes deployments and stay within budget
               constraints. With years of experience in the field, we have
               developed a user-friendly tool that provides cost estimates,
-              cluster metric data visualization, and valuable insights. 
+              cluster metric data visualization, and valuable insights.
             </p>
           </div>
           <div className="grid gap-6 px-4 sm:px-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
-              <Image
-                className=""
-                src={josh}
-                alt="Logo"
-                width={425}
-                // height={}
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                  <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
-                  Joshuah Edwards
-                  </h4>
-                  <span className="block text-sm text-gray-500">
-                    CEO-Founder
-                  </span>
+            <Link
+              href="https://www.linkedin.com/in/joshuah-edwards/"
+              target="_blank"
+            >
+              <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
+                <Image
+                  className=""
+                  src={josh}
+                  alt="Logo"
+                  width={425}
+                  // height={}
+                />
+
+                <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                  <div>
+                    <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
+                      Joshuah Edwards
+                    </h4>
+                    <span className="block text-sm text-gray-500">Founder</span>
+                  </div>
+                  <p className="mt-8 text-gray-300 dark:text-gray-600">
+                    Software engineer that appreciates the art and logic of the
+                    industry, with a passion for building, automation, and
+                    health tech.
+                  </p>
                 </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                  Quae labore quia tempora dolor impedit. Possimus, sint ducimus
-                  ipsam?
-                </p>
               </div>
-            </div>
-            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
-              <Image
-                className=""
-                src={will}
-                alt="Logo"
-                width={425}
-                // height={}
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                  <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
-                  Will Moody
-                  </h4>
-                  <span className="block text-sm text-gray-500">
-                    CEO-Founder
-                  </span>
+            </Link>
+
+            <Link
+              href="https://www.linkedin.com/in/william-moody/"
+              target="_blank"
+            >
+              <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
+                <Image
+                  className=""
+                  src={will}
+                  alt="Logo"
+                  width={425}
+                  // height={}
+                />
+                <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                  <div className="max-h-20 hover:max-h-full hover:overflow-auto">
+                    <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
+                      Will Moody
+                    </h4>
+                    <span className="block text-sm text-gray-500">Founder</span>
+                  </div>
+                  <p className="mt-8 text-gray-300 dark:text-gray-600">
+                    Software engineer with a passion for developing cutting-edge
+                    technology and building and testing full-stack applications.
+                  </p>
                 </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                  Quae labore quia tempora dolor impedit. Possimus, sint ducimus
-                  ipsam?
-                </p>
               </div>
-            </div>
-            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
-              <Image
-                className=""
-                src={ember}
-                alt="Logo"
-                width={425}
-                // height={}
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                  <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
-                  Sarah Chow
-                  </h4>
-                  <span className="block text-sm text-gray-500">
-                    CEO-Founder
-                  </span>
+            </Link>
+            <Link href="https://www.linkedin.com/in/xtchow/" target="_blank">
+              <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
+                <Image
+                  className=""
+                  src={ember}
+                  alt="Logo"
+                  width={425}
+                  // height={}
+                />
+                <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                  <div>
+                    <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
+                      Sarah Chow
+                    </h4>
+                    <span className="block text-sm text-gray-500">Founder</span>
+                  </div>
+                  <p className="mt-8 text-gray-300 dark:text-gray-600">
+                    Quae labore quia tempora dolor impedit. Possimus, sint
+                    ducimus ipsam?
+                  </p>
                 </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                  Quae labore quia tempora dolor impedit. Possimus, sint ducimus
-                  ipsam?
-                </p>
               </div>
-            </div>
-            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
-              <Image
-                className=""
-                src={alina}
-                alt="Logo"
-                width={425}
-                // height={}
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                  <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
-                    Alina Grafkina
-                  </h4>
-                  <span className="block text-sm text-gray-500">
-                    CEO-Founder
-                  </span>
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/alina-grafkina-955a56179/"
+              target="_blank"
+            >
+              <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
+                <Image
+                  className=""
+                  src={alina}
+                  alt="Logo"
+                  width={425}
+                  // height={}
+                />
+                <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-slate-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                  <div>
+                    <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
+                      Alina Grafkina
+                    </h4>
+                    <span className="block text-sm text-gray-500">Founder</span>
+                  </div>
+                  <p className="mt-8 text-gray-300 dark:text-gray-600">
+                    Innovative software engineer with a blend of technical and
+                    creative expertise.
+                  </p>
                 </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                Innovative software engineer with a blend of technical and creative expertise.
-                </p>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
